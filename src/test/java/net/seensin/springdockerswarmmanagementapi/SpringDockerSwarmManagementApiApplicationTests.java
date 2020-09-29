0ac49419ecc9d32d396a23ad0d77d61247860467
mohammad khalili <smkhalili.96@gmail.com>
2020-09-29 10:52:29 +0330

    slf4j logging: logstash + console + file logging added

---
+++ src/test/java/net/seensin/springdockerswarmmanagementapi/SpringDockerSwarmManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.seensin.springdockerswarmmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerSwarmManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
